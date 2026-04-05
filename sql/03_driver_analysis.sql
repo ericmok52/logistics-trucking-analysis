15435f5881bd431391389b4e2009c5144a8da2e3
ericmok52 <100112594+ericmok52@users.noreply.github.com>
2026-04-06 01:32:30 +1000

    Update 03_driver_analysis.sql

--- sql/03_driver_analysis.sql
+++ sql/03_driver_analysis.sql
@@ -1,1 +1,23 @@
+-- Which driver generates the most revenue?
+SELECT
+d.driver_id,
+SUM(l.revenue) AS total_revenue
+FROM drivers d
+JOIN trips t
+ON d.driver_id = t.driver_id
+JOIN loads l
+ON t.load_id = l.load_id
+GROUP BY d.driver_id
+ORDER BY total_revenue DESC;
 
+--How many trips has each driver completed?
+SELECT
+d.driver_id,
+COUNT(t.trip_id) AS total_trips
+FROM drivers d
+JOIN trips t
+ON d.driver_id = t.driver_id
+GROUP BY d.driver_id
+ORDER BY total_trips DESC;
+
+--low counts can indicate inefficiencies
