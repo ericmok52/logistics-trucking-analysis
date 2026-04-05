3e673f832eee563f9369670b9aa4beb4d76a6283
ericmok52 <100112594+ericmok52@users.noreply.github.com>
2026-04-06 01:29:07 +1000

    Add SQL queries for total revenue and top customer

--- sql/02_analysis.sql
+++ sql/02_analysis.sql
@@ -1,1 +1,26 @@
+-- Total revenue per customer
+SELECT 
+    c.customer_name,
+    c.customer_id,
+    SUM(l.revenue) AS total_revenue
+FROM customers c
+JOIN loads l
+ON c.customer_id = l.customer_id
+GROUP BY 
+    c.customer_id,
+    c.customer_name
+ORDER BY total_revenue DESC;
 
+-- Top customer by revenue
+SELECT 
+    c.customer_name,
+    c.customer_id,
+    SUM(l.revenue) AS total_revenue
+FROM customers c
+JOIN loads l
+ON c.customer_id = l.customer_id
+GROUP BY 
+    c.customer_id,
+    c.customer_name
+ORDER BY total_revenue DESC
+LIMIT 1;
